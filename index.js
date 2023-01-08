1a3cd252331f802ca218af342497521e97aaec2e
DESKTOP-1SNG5P3\anass <anasshaikh5v2@gmail.com>
2023-01-08 14:46:46 +0530

    project init

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const requests = require("requests");
+
+const htmlFile = fs.readFileSync("index.html", "utf-8");
+const getActualTemp = (val) => {
+  return Math.floor(val - 273.15);
+};
+const replaceValues = (htmlFile, values) => {
+  var updatedHtmlFile = htmlFile.replace("{%location%}", values.name);
+  updatedHtmlFile = updatedHtmlFile.replace("{%country%}", values.sys.country);
+  updatedHtmlFile = updatedHtmlFile.replace("{%tempVal%}", getActualTemp(values.main.temp));
+  updatedHtmlFile = updatedHtmlFile.replace("{%tempMin%}", getActualTemp(values.main.temp_min));
+  updatedHtmlFile = updatedHtmlFile.replace("{%tempMax%}", getActualTemp(values.main.temp_max));
+  updatedHtmlFile = updatedHtmlFile.replace("{%tempratureStatus%}", values.weather[0].main);
+  console.log(values.weather[0].main);
+  return updatedHtmlFile;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests("https://api.openweathermap.org/data/2.5/weather?q=Ahmedabad&appid=b55acd48f02be4cf30c113683a1650ac")
+      .on("data", (chunk) => {
+        const response = JSON.parse(chunk);
+        const realTimeData = replaceValues(htmlFile, response);
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) throw err;
+        res.end();
+      });
+  } else {
+    res.end("404 not found");
+  }
+});
+
+server.listen(9000, "127.0.0.1");
